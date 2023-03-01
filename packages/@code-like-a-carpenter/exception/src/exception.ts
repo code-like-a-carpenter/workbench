3a82b1bccdeef3c8984355a3edef94427626745a
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2023-02-28 20:52:03 -0800

    fix(exception): render proper name

--- packages/@code-like-a-carpenter/exception/src/exception.ts
+++ packages/@code-like-a-carpenter/exception/src/exception.ts
@@ -1,12 +1,23 @@
-interface ExceptionOptions<T extends object> extends ErrorOptions {
+export interface ExceptionOptions<T extends object> extends ErrorOptions {
   telemetry?: T;
 }
 
 export class Exception<T extends object> extends Error {
   readonly telemetry?: T;
 
-  constructor(message: string, {telemetry, ...options}: ExceptionOptions<T>) {
+  constructor(
+    message: string,
+    {telemetry, ...options}: ExceptionOptions<T> = {}
+  ) {
     super(message, options);
     this.telemetry = telemetry;
+  }
+
+  /**
+   * Override name property so we get the real constructor name instead of just
+   * "Error"
+   */
+  get name() {
+    return this.constructor.name;
   }
 }
