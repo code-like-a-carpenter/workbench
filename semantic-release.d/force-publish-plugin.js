dd281cb0dd1bae32d98932dde2cb4b5d201d6b16
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2023-03-09 07:25:24 -0800

    ci: add logging to patch-release plugin

--- semantic-release.d/force-publish-plugin.js
+++ semantic-release.d/force-publish-plugin.js
@@ -1,7 +1,11 @@
 'use strict';
 
 exports.analyzeCommits = (pluginConfig, context) => {
-  if (context.commits[0].message === 'chore: patch-release') {
+  console.info('checking for patch-release commit message');
+  console.info(require(`util`).inspect({context}, {depth: null}));
+  if (context.commits.any(({message}) => message === 'chore: patch-release')) {
+    console.info('patch-release commit message found');
     return 'patch';
   }
+  console.info('no patch-release commit message found');
 };
