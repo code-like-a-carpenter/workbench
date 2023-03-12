6610c0735d27a7976239ce079e5af1657269302d
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2023-03-12 16:00:10 -0700

    fix(telemetry): only bind handlers once, not once per request

--- packages/@code-like-a-carpenter/telemetry/src/instrumentation/sqs.ts
+++ packages/@code-like-a-carpenter/telemetry/src/instrumentation/sqs.ts
@@ -34,54 +34,54 @@ const sharedLinks = new WeakMap<SQSRecord, Link>();
 export function instrumentSQSHandler(
   handler: NoVoidSQSHandler
 ): NoVoidSQSHandler {
+  // @ts-expect-error: Sentry uses the generalized AWS Handler type, which
+  // allows for nodeback-style handlers.
+  const sentryWrappedHandler: NoVoidSQSHandler = AWSLambda.wrapHandler(handler);
+
   let cold = true;
   return async (event, context) => {
-    const wasCold = cold;
-    cold = false;
-
-    // @ts-expect-error: Sentry uses the generalized AWS Handler type, which
-    // allows for nodeback-style handlers.
-    const sentryWrappedHandler: NoVoidSQSHandler =
-      AWSLambda.wrapHandler(handler);
-
-    const attributes: Attributes = {
-      'aws.lambda.invoked_arn': context.invokedFunctionArn,
-      'cloud.account.id': context.invokedFunctionArn.split(':')[5],
-      'faas.coldstart': wasCold,
-      'faas.execution': context.awsRequestId,
-      'faas.id': `${context.invokedFunctionArn
-        .split(':')
-        .slice(0, 7)
-        .join(':')}:${context.functionVersion}`,
-
-      'faas.trigger': 'pubsub',
-      'messaging.operation': 'process',
-      'messaging.source.kind': 'queue',
-      'messaging.system': 'AmazonSQS',
-    };
-
-    const links = new Map<SQSRecord, Link>();
-    const eventSources = new Set<string>();
-
-    for (const record of event.Records) {
-      const traceHeader = record.messageAttributes?.AWSTraceHeader?.stringValue;
-      if (traceHeader) {
-        const ctx = propagation.extract(contextAPI.active(), traceHeader);
-        const spanCtx = trace.getSpanContext(ctx);
-        if (spanCtx) {
-          links.set(record, {context: spanCtx});
-          sharedLinks.set(record, {context: spanCtx});
-        }
-      }
-      eventSources.add(record.eventSource);
-    }
-
-    const eventSource =
-      eventSources.size === 1
-        ? event.Records[0].eventSource
-        : 'multiple_sources';
-
     try {
+      const wasCold = cold;
+      cold = false;
+
+      const attributes: Attributes = {
+        'aws.lambda.invoked_arn': context.invokedFunctionArn,
+        'cloud.account.id': context.invokedFunctionArn.split(':')[5],
+        'faas.coldstart': wasCold,
+        'faas.execution': context.awsRequestId,
+        'faas.id': `${context.invokedFunctionArn
+          .split(':')
+          .slice(0, 7)
+          .join(':')}:${context.functionVersion}`,
+
+        'faas.trigger': 'pubsub',
+        'messaging.operation': 'process',
+        'messaging.source.kind': 'queue',
+        'messaging.system': 'AmazonSQS',
+      };
+
+      const links = new Map<SQSRecord, Link>();
+      const eventSources = new Set<string>();
+
+      for (const record of event.Records) {
+        const traceHeader =
+          record.messageAttributes?.AWSTraceHeader?.stringValue;
+        if (traceHeader) {
+          const ctx = propagation.extract(contextAPI.active(), traceHeader);
+          const spanCtx = trace.getSpanContext(ctx);
+          if (spanCtx) {
+            links.set(record, {context: spanCtx});
+            sharedLinks.set(record, {context: spanCtx});
+          }
+        }
+        eventSources.add(record.eventSource);
+      }
+
+      const eventSource =
+        eventSources.size === 1
+          ? event.Records[0].eventSource
+          : 'multiple_sources';
+
       return await runWithNewSpan(
         `${eventSource} process`,
         {
