2d6acd1eac5cbecaa3fd92b510932c940b174cd3
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2023-03-12 14:24:01 -0700

    feat(lambda-handlers): add token authorizer

--- examples/aws-authorizer/src/authorizer.ts
+++ examples/aws-authorizer/src/authorizer.ts
@@ -4,6 +4,10 @@ import {
   PaymentRequired,
   Unauthorized,
 } from '@code-like-a-carpenter/errors';
+import {
+  generatePolicy,
+  handleRestTokenAuthorizerEvent,
+} from '@code-like-a-carpenter/lambda-handlers';
 
 type AuthorizerContext =
   | {
@@ -16,68 +20,45 @@ type AuthorizerContext =
       password: string;
     };
 
-export const authorize = async (event) => {
-  const [scheme, token] = event.authorizationToken.split(' ');
+export const authorize = handleRestTokenAuthorizerEvent<AuthorizerContext>(
+  async (event) => {
+    const [scheme, token] = event.authorizationToken.split(' ');
 
-  if (scheme === 'Bearer') {
-    if (token === '401') {
-      throw new Unauthorized('Invalid or missing token');
-    }
-    if (token === '402') {
-      throw new PaymentRequired('You must pay to access this resource');
-    }
-    if (token === '403') {
-      throw new Forbidden('You are not authorized to access this resource');
-    }
-    if (token === '404') {
-      throw new NotFound('Could not find specified resource');
-    }
-
-    return generatePolicy('Allow', event.methodArn, {
-      scheme,
-      token,
-    });
-  }
-
-  if (scheme === 'Basic') {
-    const [user, password] = Buffer.from(token, 'base64')
-      .toString('utf8')
-      .split(':');
-
-    return generatePolicy('Allow', event.methodArn, {
-      password,
-      scheme,
-      user,
-    });
-  }
-
-  return generatePolicy('Deny', event.methodArn, {
-    message: `${scheme} must be one of 'Bearer' or 'Basic'`,
-  });
-};
-
-// Help function to generate an IAM policy
-function generatePolicy<E extends 'Allow' | 'Deny'>(
-  effect: E,
-  resource: string,
-  context: E extends 'Allow'
-    ? AuthorizerContext
-    : {
-        message: string;
+    if (scheme === 'Bearer') {
+      if (token === '401') {
+        throw new Unauthorized('Invalid or missing token');
       }
-) {
-  return {
-    context,
-    policyDocument: {
-      Statement: [
-        {
-          Action: 'execute-api:Invoke',
-          Effect: effect,
-          Resource: '*',
+      if (token === '402') {
+        throw new PaymentRequired('You must pay to access this resource');
+      }
+      if (token === '403') {
+        throw new Forbidden('You are not authorized to access this resource');
+      }
+      if (token === '404') {
+        throw new NotFound('Could not find specified resource');
+      }
+
+      return {
+        context: {
+          scheme,
+          token,
         },
-      ],
-      Version: '2012-10-17',
-    },
-    principalId: effect === 'Allow' ? 'authenticated' : 'anonymous',
-  };
-}
+        principalId: 'authenticated',
+      };
+    }
+
+    if (scheme === 'Basic') {
+      const [user, password] = Buffer.from(token, 'base64')
+        .toString('utf8')
+        .split(':');
+
+      return generatePolicy('Allow', event.methodArn, {
+        password,
+        scheme,
+        user,
+      });
+    }
+
+    throw new Unauthorized(`${scheme} must be one of 'Bearer' or 'Basic'`);
+  }
+);
--- packages/@code-like-a-carpenter/lambda-handlers/src/index.ts
+++ packages/@code-like-a-carpenter/lambda-handlers/src/index.ts
@@ -1,2 +1,3 @@
 export * from './rest';
+export * from './rest-token-authorizer';
 export * from './sqs';
