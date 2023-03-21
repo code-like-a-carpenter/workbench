a2e27c20d82c29dd2bdb9fe9010f4c7896ba7b90
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2023-03-20 21:02:52 -0700

    ci: configure graphql linting

---
+++ .graphqlrc.js
@@ -0,0 +1,29 @@
+const fs = require('node:fs');
+
+const {sync: glob} = require('glob');
+
+const examples = glob('*/', {cwd: 'examples'}).map((pathName) =>
+  pathName.replace(/\/$/, '')
+);
+
+/** @type {Record<string, import("graphql-config").IGraphQLProject>} */
+const init = {};
+
+/** @type {import("graphql-config").IGraphQLConfig } */
+const config = {
+  projects: examples.reduce((acc, example) => {
+    acc[example] = {
+      schema: [
+        `examples/${example}/schema/**/*.graphqls`,
+        // This line loads types necessary for the collective schema to be valid.
+        'examples/common.graphqls',
+        // This line is temporary and should be removed once the schema is
+        // supplied via addToSchema
+        'schema.graphqls',
+      ],
+    };
+    return acc;
+  }, init),
+};
+
+module.exports = config;
