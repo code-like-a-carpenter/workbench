83fee1826cc0041aab9f666ecf19f99845fca147
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2023-11-07 21:28:19 -0800

    feat(lambda-handlers): add optional zod schema validation

--- packages/@code-like-a-carpenter/lambda-handlers/src/rest/rest.ts
+++ packages/@code-like-a-carpenter/lambda-handlers/src/rest/rest.ts
@@ -1,9 +1,13 @@
 import type {APIGatewayProxyHandler} from 'aws-lambda';
+import type {Schema} from 'zod';
+import {ZodError} from 'zod';
+import {fromZodError} from 'zod-validation-error';
 
 import {logger as rootLogger} from '@code-like-a-carpenter/logger';
 import {instrumentRestHandler} from '@code-like-a-carpenter/telemetry';
 
 import {
+  formatEarlyErrorResult,
   formatErrorResult,
   formatEvent,
   formatSuccessResult,
@@ -14,10 +18,32 @@ import type {RestCallback, SimplifiedOperationObject} from './types';
  * Creates a handler for an API Gateway REST API (aka, an API Gateway V1 API).
  */
 export function handleRestEvent<O extends SimplifiedOperationObject>(
-  callback: RestCallback<O>
+  callback: RestCallback<O>,
+  schema?: Schema
 ): APIGatewayProxyHandler {
   return instrumentRestHandler(async (event, context) => {
-    const restEvent = formatEvent(event);
+    let restEvent;
+    try {
+      restEvent = formatEvent(event, schema);
+    } catch (err) {
+      if (err instanceof ZodError) {
+        return {
+          body: JSON.stringify({
+            error: err,
+            errors: err.errors,
+            message: fromZodError(err).toString(),
+            name: err.name,
+            requestIds: {
+              awsRequestId: context.awsRequestId,
+              requestId: event.requestContext.requestId,
+              xAmznTraceId: event.headers?.['X-Amzn-Trace-Id'],
+            },
+          }),
+          statusCode: 400,
+        };
+      }
+      return formatEarlyErrorResult(err, event, context);
+    }
 
     const logger = rootLogger.child({});
 
