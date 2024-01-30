af56f2a1408681f5da3d25a1dae084c2c133c3d7
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2024-01-29 20:43:17 -0800

    feat(cli): add cli framework

---
+++ packages/@code-like-a-carpenter/cli/cli.mjs
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+'use strict';
+
+import('@code-like-a-carpenter/cli');
---
+++ packages/@code-like-a-carpenter/cli/src/index.ts
@@ -0,0 +1,6 @@
+import {main} from '@code-like-a-carpenter/cli-core';
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
